0869752df348532d9478ff3587ed3c29f82636dc
Portal <juanportalg@gmail.com>
2023-05-10 22:05:45 -0700

    from scratch

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-This is my personal website, it's not perfect but I'm just starting in the web development world. Feel free to use it to build yours!
-
-![image](https://user-images.githubusercontent.com/78442505/164917031-dfbb6437-8978-4acf-bb93-f94a0025643e.png)
+This is a website dedicated to my Discord Bots
---
+++ script.js
@@ -0,0 +1,21 @@
+// Redirecting to my portfolio
+document.querySelector('footer span').addEventListener('click', () => {
+    window.open('http://juanportalg.com/', '_blank');
+})
+
+// Inviting the bots to a server
+document.querySelector('.bot-card:nth-child(1)').addEventListener('click', () => {
+    window.open('https://discord.com/api/oauth2/authorize?client_id=806597591231889499&permissions=519232&scope=bot', '_blank');
+})
+
+document.querySelector('.bot-card:nth-child(2)').addEventListener('click', () => {
+    window.open('https://discord.com/api/oauth2/authorize?client_id=805485199064432683&permissions=380141567232&scope=bot', '_blank');
+})
+
+document.querySelector('.bot-card:nth-child(3)').addEventListener('click', () => {
+    window.open('https://discord.com/api/oauth2/authorize?client_id=820069086985453608&permissions=2148006976&scope=bot', '_blank');
+})
+
+document.querySelector('.bot-card:nth-child(4)').addEventListener('click', () => {
+    window.open('https://discord.com/api/oauth2/authorize?client_id=812473364844511273&permissions=68608&scope=bot', '_blank');
+})
